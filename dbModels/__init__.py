7df6300bc3c6d7b71621cd1f37a796db5fe6b935
Nizar Bahajoub <n.bahajoub@gmail.com>
2025-01-07 17:32:33 +0100

    Models Classes Created

---
+++ dbModels/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '0d3034043c078b60e6d43b5b918c0227'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:''@localhost/absence'
+db = SQLAlchemy(app)
